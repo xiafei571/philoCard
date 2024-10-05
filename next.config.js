cf2e01d48157646a123e5a0f2bc979aa2657cab7
xia fei <xiafei571@gmail.com>
2024-10-05 18:04:14 +0900

    移除敏感数据

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-  headers: async () => [
-    {
-      source: '/images/src/:path*',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'no-store, must-revalidate',
-        },
-      ],
-    },
-  ],
-}
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      console.log('Client-side environment variables:');
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+      // ... 打印其他环境变量
+    }
+    return config;
+  },
+};
